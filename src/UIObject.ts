788a22e2d07211b46b4c36637e48162b167f9a7c
cccgh5 <47964442+cccgh5@users.noreply.github.com>
2024-09-03 16:15:22 +0900

    Update UIObject.ts

--- src/UIObject.ts
+++ src/UIObject.ts
@@ -2,11 +2,11 @@ import { RenderParameters } from './rouletteRenderer';
 import {Rect} from './types/rect.type';
 
 export interface UIObject {
+    print({winners, marbles, winnerRank}: RenderParameters): void;
     update(deltaTime: number): void;
     render(ctx: CanvasRenderingContext2D, params: RenderParameters, width: number, height: number): void;
     getBoundingBox(): Rect | null;
-    print({winners, marbles, winnerRank}: RenderParameters): void;
-
+    
     onWheel?(e: WheelEvent): void;
     onMouseMove?(e?: {x: number, y: number}): void;
     onMouseDown?(e: {x: number, y: number}): void;
