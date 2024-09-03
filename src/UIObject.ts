f600c2a0c9db6cb423240db785118b70ce9cfd91
cccgh5 <47964442+cccgh5@users.noreply.github.com>
2024-09-03 15:59:36 +0900

    Update UIObject.ts

--- src/UIObject.ts
+++ src/UIObject.ts
@@ -5,6 +5,7 @@ export interface UIObject {
     update(deltaTime: number): void;
     render(ctx: CanvasRenderingContext2D, params: RenderParameters, width: number, height: number): void;
     getBoundingBox(): Rect | null;
+    print({winners, marbles, winnerRank}: RenderParameters): void;
 
     onWheel?(e: WheelEvent): void;
     onMouseMove?(e?: {x: number, y: number}): void;
